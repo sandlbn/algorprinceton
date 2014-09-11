79fe4d8df59c2cc6add53a0f8b4efe5849a71a7f
Marcin Spoczynski <marcin@spoczynski.com>
2014-09-11 19:45:08 +0100

    Added Union Find compressed

---
+++ src/prinston15/QuickUnionCompression.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package prinston15;
+
+import java.util.Random;
+
+/**
+ * @author sand
+ */
+
+public class QuickUnionCompression {
+    private int[] id;
+    private int[] sz;
+
+    public QuickUnionCompression(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+        sz = new int[N];
+        for (int i = 0; i < N; i++) {
+            sz[i] = 1;
+        }
+    }
+
+    private int root(int i) {
+        while (i != id[i]) {
+            id[i] = id[id[i]];
+            i = id[i];
+        }
+        return i;
+    }
+
+    private boolean connected(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    private void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        if (j == i) return;
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = 10;
+        QuickUnionCompression qu = new QuickUnionCompression(N);
+        for (int i = 0; i < 6; i++) {
+            Random generator = new Random();
+            int p = generator.nextInt(10);
+            int q = generator.nextInt(10);
+            if (qu.connected(p, q)) continue;
+            qu.union(p, q);
+            StdOut.print("(" + p + "," + q + ")");
+            for (int j = 0; j < qu.id.length; j++) {
+                StdOut.print(qu.id[j] + ", ");
+            }
+            StdOut.println();
+            for (int j = 0; j < qu.sz.length; j++) {
+                StdOut.print(qu.sz[j] + ", ");
+            }
+            StdOut.println();
+
+        }
+        for (int j = 0; j < qu.id.length; j++) {
+            StdOut.print(qu.id[j] + ", ");
+        }
+    }
+
+}
